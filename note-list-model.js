446816a26ea92ffa0fb9f80b955f862ce3d5865a
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2017-05-22 18:41:51 +0100

    adds new note method

--- note-list-model.js
+++ note-list-model.js
@@ -4,6 +4,11 @@ function NoteList () {
   this.giveAllNotes = function () {
     return this.allNotes;
   };
+  this.newNote = function(string) {
+    note = new Note()
+    note.setText(string)
+    this.allNotes.push(note)
+  }
 };
 
 exports.NoteList = NoteList;
--- noteListTest.js
+++ noteListTest.js
@@ -3,4 +3,12 @@ function giveAllNotesIsAnEmptyArray() {
   assert.isTrue(noteList.giveAllNotes().length == 0)
 }
 
+function newNoteSetsPropertyOfNote(){
+  var noteList = new NoteList ()
+  var string  = 'an array is not an array'
+  noteList.newNote(string)
+  assert.isTrue(noteList.allNotes[0].text === string)
+}
+
 giveAllNotesIsAnEmptyArray();
+newNoteSetsPropertyOfNote();
