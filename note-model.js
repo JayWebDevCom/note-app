f5696144cfe7716df48e1ded9f13f014cd5e899c
JayWebDevCom <jaiye.s@gmail.com>
2017-05-23 21:50:52 +0100

    starting over possible error somewhere

--- note-model.js
+++ note-model.js
@@ -1,10 +1,12 @@
 (function(exports) {
 function Note(){
   this.text = ''
-  this.giveText = function(){
-    return this.text;
-  };
+}
+
+Note.prototype.giveText = function(){
+  return this.text;
 };
+
 Note.prototype.setText = function (string) {
   this.text = string
 };
