a7d037498155c6ec43b8ade3bc9f294adf15e967
JayWebDevCom <jaiye.s@gmail.com>
2017-05-23 16:16:18 +0100

    note list controller - static

--- note-controller.js
+++ note-controller.js
@@ -1,2 +1,30 @@
-var app = document.getElementById("app")
-app.innerHTML = "Bob is not a goat, he's an elephant !!"
+
+
+(function(exports){
+
+function NoteController (noteList = new NoteList()) {
+
+  noteList = new NoteList()
+
+  noteList.newNote('this is the string of the note')
+    
+  var noteView = new NoteView(noteList)
+
+  this.printNotes = function () {
+
+    var stringToReturn = noteView.outputNotes()
+    var app = document.getElementById("app")
+    app.innerHTML = stringToReturn
+
+  }
+
+}
+
+
+
+exports.NoteController = NoteController;
+})(this)
+
+
+var noteController = new NoteController();
+noteController.printNotes()
