a0248c93c40382904fd2ff2dc1d0918a461a4b4f
JayWebDevCom <jaiye.s@gmail.com>
2017-05-23 23:05:14 +0100

    add note-controller spiked

--- note-controller.js
+++ note-controller.js
@@ -1,20 +1,26 @@
 (function(exports){
 
 function NoteController (noteList) {
-
   noteList.newNote('this is the string of the note two');
+  this._myNoteListView = new NoteListView(noteList)
+}
 
-  var noteView = new NoteView(noteList)
+NoteController.prototype.printNotes = function () {
+  var stringToReturn = this._myNoteListView.outputNotes()
+  var app = document.getElementById("app")
+  app.innerHTML = stringToReturn
+}
 
-  this.printNotes = function () {
-
-    var stringToReturn = noteView.outputNotes()
-    var app = document.getElementById("app")
-    app.innerHTML = stringToReturn
-
-  }
+NoteController.prototype.adjustPage = function () {
+  var app = document.getElementById("app")
+  app.innerHTML = 'new content'
 
 }
 
 exports.NoteController = NoteController;
 })(this)
+
+noteList = new NoteList();
+noteController = new NoteController(noteList);
+
+noteController.printNotes()
